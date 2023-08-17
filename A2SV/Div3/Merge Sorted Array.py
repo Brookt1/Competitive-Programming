b6af9fc340286944284be78989e702af3141b6a4
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2023-08-17 15:38:30 +0300

    LC problem with python

---
+++ A2SV/Div3/Merge Sorted Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        k,fir,sec=m+n-1,m-1,n-1
+        while sec>=0 and fir>=0:
+            if nums2[sec]>nums1[fir]:
+                nums1[k]=nums2[sec]
+                sec-=1
+            else:
+                nums1[k]=nums1[fir]
+                fir-=1
+            k-=1
+        while sec>=0:
+            nums1[k]=nums2[sec]
+            k-=1
+            sec-=1
