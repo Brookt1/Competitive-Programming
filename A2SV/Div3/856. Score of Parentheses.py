b0bdc1caf5738e94b3bd22fcf9200ad8aaa34b5e
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2023-09-08 15:14:42 +0300

    LC problem with python

---
+++ A2SV/Div3/856. Score of Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack=[]
+
+        score=0
+        for chr in s:
+
+            if chr =="(":
+                stack.append(score)
+                score=0
+            else:
+                score=stack.pop() + max(score*2,1)
+        
+        return score
+
