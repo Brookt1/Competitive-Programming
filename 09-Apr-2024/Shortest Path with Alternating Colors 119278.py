4a5e51bc57b5b6c230b6234576796eaf273f57be
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-09 19:56:09 +0300

    Created 'Shortest Path with Alternating Colors' by A2SV Hub

---
+++ 09-Apr-2024/Shortest Path with Alternating Colors 119278.py
@@ -0,0 +1,35 @@
+# Problem: Shortest Path with Alternating Colors - https://leetcode.com/problems/shortest-path-with-alternating-colors/description/
+
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        queue = deque()
+        visited = set()
+        red_edges = defaultdict(list)
+        blue_edges = defaultdict(list)
+        for a,b in redEdges:
+            red_edges[a].append(b)
+        for a, b in blueEdges:
+            blue_edges[a].append(b)
+        queue.append([0, 1, 0])
+        visited.add((0, 1))
+        queue.append([0, 2, 0])
+        visited.add((0, 2))
+        ans = [-1]*n
+        while queue:
+            for _ in range(len(queue)):
+                node, color, depth = queue.popleft()
+                if ans[node] == -1:
+                    ans[node] = depth
+                if color == 1:
+                    for adj in red_edges[node]:
+                        val = (adj, 1)
+                        if val not in visited:
+                            queue.append([adj, 2, depth + 1])
+                            visited.add(val)
+                else:
+                    for adj in blue_edges[node]:
+                        val = (adj, 2)
+                        if val not in visited:
+                            queue.append([adj, 1, depth + 1])
+                            visited.add(val)
+        return ans
