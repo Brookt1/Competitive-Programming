fe76ec0806752b707756a6f5d62760f03bb18aa4
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-09 09:28:24 +0300

    Created 'A - Flag Checker' by A2SV Hub

---
+++ A2SV G5 - Contest #11 09-Apr-2024/A - Flag Checker 118948.py
@@ -0,0 +1,30 @@
+# Problem: A - Flag Checker - https://codeforces.com/gym/515998/problem/A
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    n, m = intl()
+    colors = []
+    flag = True
+    for _ in range(n):
+        line = input()
+        num = list(set(line))
+        if len(line) != m or len(num) != 1 or colors and num[0] == colors[-1]:
+            flag = False
+            break
+        else:
+            colors.append(num[0])
+    if flag:
+        print('YES')
+    else:
+        print('NO')
+            
+
+
+solve()
+
