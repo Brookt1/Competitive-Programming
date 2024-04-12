e9c758e4b314b72efee297bd24d483ae4eb65e36
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-12 10:15:27 +0300

    Created 'Turtle vs. Rabbit Race: Optimal Trainings' by A2SV Hub

---
+++ 12-Apr-2024/Turtle vs. Rabbit Race: Optimal Trainings 121264.py
@@ -0,0 +1,36 @@
+# Problem: Turtle vs. Rabbit Race: Optimal Trainings - https://codeforces.com/contest/1933/problem/E
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    n = int(input())
+    nums = intl()
+    prefix = [0]
+    for num in nums:
+        prefix.append(prefix[-1] + num)
+    ans = []
+    for _ in range(int(input())):
+        left, u = intl()
+        right = bisect_left(prefix, u + prefix[left - 1])
+        if right >= len(prefix):
+            right -= 1
+        elif right > left:
+            diff = prefix[right] - prefix[left - 1] - u
+            if diff >= 0:
+                sum_one = (u*(u + 1)//2) - (abs(diff)*abs(diff - 1)//2)
+                rem = u - (prefix[right - 1] - prefix[left - 1])
+                sum_two = (u*(u + 1)//2) - (rem*(rem + 1)//2)
+                if sum_two >= sum_one:
+                    right -= 1
+        ans.append(right)
+    print(*ans)
+
+t = int(input())
+for _ in range(t):
+    solve()
+
