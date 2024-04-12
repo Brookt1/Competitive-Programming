254d8ad9410e56a24b63ad5408f13743f4c23a4d
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-12 18:21:27 +0300

    Created 'Divide and equalize' by A2SV Hub

---
+++ 12-Apr-2024/Divide and equalize 121641.py
@@ -0,0 +1,39 @@
+# Problem: Divide and equalize - https://codeforces.com/problemset/problem/1881/D
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys, math
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+
+def solve():
+    n = int(input())
+    nums = intl()
+    count = defaultdict(int)
+    for x in nums:
+        num = x
+        while num % 2 == 0:
+            num //= 2
+            count[2] += 1
+        
+        for i in range(3, int(math.sqrt(num)) + 1, 2):
+            while num % i == 0:
+                num //= i
+                count[i] += 1
+        if num > 1:
+            count[num] += 1
+    flag = False
+    for val in count.values():
+        if val % n != 0:
+            flag = True
+            break
+    if flag:
+        print('NO')
+    else:
+        print('YES')
+    
+t = int(input())
+for _ in range(t):
+    solve()
