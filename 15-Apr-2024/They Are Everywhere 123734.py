adf2543347d6f78051c5bd79caab6329066ef8fb
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-15 21:08:06 +0300

    Created 'They Are Everywhere' by A2SV Hub

---
+++ 15-Apr-2024/They Are Everywhere 123734.py
@@ -0,0 +1,30 @@
+# Problem: They Are Everywhere - https://codeforces.com/problemset/problem/701/C
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    n = int(input())
+    s = input()
+    size = len(set(s))
+    left = right = 0
+    count = defaultdict(int)
+    ans = n + 1
+    for right in range(n):
+        count[s[right]] += 1
+        while len(count) == size:
+            ans = min(ans, right - left + 1)
+            count[s[left]] -= 1
+            if count[s[left]] == 0:
+                del count[s[left]]
+            left += 1
+    print(ans)
+
+#t = int(input())
+#for _ in range(t):
+solve()
+
