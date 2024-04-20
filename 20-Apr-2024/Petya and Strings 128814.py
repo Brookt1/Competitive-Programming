a5a083242af9b670f21b78e67f6d9dd1ccae03d1
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-20 20:07:01 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 20-Apr-2024/Petya and Strings 128814.py
@@ -0,0 +1,24 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    s1 = input().lower()
+    s2 = input().lower()
+    if s1 == s2:
+        print(0)
+    elif s1 > s2:
+        print(1)
+    else:
+        print(-1)
+
+
+#t = int(input())
+#for _ in range(t):
+solve()
+
