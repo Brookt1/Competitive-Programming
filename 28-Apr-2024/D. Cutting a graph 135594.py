1bed7373a7e9da5fa9ab7af0c6dca23f424e76b8
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-28 20:10:56 +0300

    Created 'D. Cutting a graph' by A2SV Hub

---
+++ 28-Apr-2024/D. Cutting a graph 135594.py
@@ -0,0 +1,54 @@
+# Problem: D. Cutting a graph - https://codeforces.com/edu/course/2/lesson/7/1/practice/contest/289390/problem/D
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    
+    def find(x):
+        while x != root[x]:
+            root[x] = root[root[x]]
+            x = root[x]
+        return x
+    def union(x, y):
+        rootX = find(x)
+        rootY = find(y)
+        if rootX != rootY:
+            if rank[rootX] > rank[rootY]:
+                root[rootY] = rootX
+            elif rank[rootX] < rank[rootY]:
+                root[rootX] = rootY
+            else:
+                rank[rootX] += 1
+                root[rootY] = rootX
+    
+    n, m, k = intl()
+    for _ in range(m):
+        input()
+    oper = []
+    for k in range(k):
+        oper.append(input().split())
+
+    root = [i for i in range(n)]
+    rank = [0]*n
+    ans = []
+    for c, a, b in oper[::-1]:
+        if c == 'ask':
+            if find(int(a) - 1) == find(int(b) - 1):
+                ans.append('YES')
+            else:
+                ans.append('NO')
+        else:
+            union(int(a) - 1, int(b) - 1)
+    for l in ans[::-1]:
+        print(l)
+
+
+#t = int(input())
+#for _ in range(t):
+solve()
+
