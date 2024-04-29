f939fffd5694284d96c51b8ce50f0c47ca163a92
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-04-29 13:59:04 +0300

    Created 'Disjoint Sets Union 2' by A2SV Hub

---
+++ 29-Apr-2024/Disjoint Sets Union 2 135876.py
@@ -0,0 +1,53 @@
+# Problem: Disjoint Sets Union 2 - https://codeforces.com/edu/course/2/lesson/7/1/practice/contest/289390/problem/B
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    
+    def find(x):
+        while x != root[x]:
+            root[x] = root[root[x]]
+            x = root[x]
+        return x
+    def union(a, b):
+        rootA = find(a)
+        rootB = find(b)
+        if rootA != rootB:
+            if size[rootA] > size[rootB]:
+                _min, _max = data[rootB - 1]
+                data[rootA - 1] = [min(data[rootA - 1][0], _min), max(data[rootA - 1][1], _max)]
+                size[rootA] += size[rootB]
+                root[rootB] = rootA 
+            else: 
+                _min, _max = data[rootA - 1]
+                data[rootB - 1] = [min(data[rootB - 1][0], _min), max(data[rootB - 1][1], _max)]
+                size[rootB] += size[rootA]
+                root[rootA] = rootB
+
+
+    
+    n, m = intl()
+    root = {i:i for i in range(1, n + 1)}
+    size = {i:1 for i in range(1, n + 1)}
+    data = [[i, i] for i in range(1, n + 1)]
+    ans = [ ]
+    for _ in range(m):
+        line = input().split()
+        if len(line) > 2:
+            union(int(line[1]), int(line[2]))
+        else:
+            ans.append(data[find(int(line[1])) - 1] + [size[find(int(line[1]))]])
+    for l in ans:
+        print(*l)
+    
+
+
+#t = int(input())
+#for _ in range(t):
+solve()
+
