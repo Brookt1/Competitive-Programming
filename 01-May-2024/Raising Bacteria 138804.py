5e33ebf8810ab24806ca7c3ba10d82ad45ecb6d6
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-05-01 23:11:05 +0300

    Created 'Raising Bacteria' by A2SV Hub

---
+++ 01-May-2024/Raising Bacteria 138804.py
@@ -0,0 +1,17 @@
+# Problem: Raising Bacteria - https://codeforces.com/contest/579/problem/A
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    n = int(input())
+    print(n.bit_count())
+
+#t = int(input())
+#for _ in range(t):
+solve()
+
