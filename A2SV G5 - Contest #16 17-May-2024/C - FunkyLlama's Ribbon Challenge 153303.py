378e403687e4b490663f71f07a7d576fb2a672a1
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-05-17 19:16:21 +0300

    Created 'C - FunkyLlama's Ribbon Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #16 17-May-2024/C - FunkyLlama's Ribbon Challenge 153303.py
@@ -0,0 +1,28 @@
+# Problem: C - FunkyLlama's Ribbon Challenge - https://codeforces.com/gym/523525/problem/C
+
+import sys,threading
+sys.setrecursionlimit(1 << 30)
+threading.stack_size(1 << 27)
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    n, a, b, c = intl()
+    memo = {}
+    def dp(n):
+        if n <= 0:
+            if n == 0:
+                return 0
+            return float('-inf')
+        if n not in memo:
+            memo[n] = max(dp(n - a) + 1, dp(n - b) + 1, dp(n - c) + 1)
+        return memo[n]
+    ans = dp(n)
+    print(ans)
+def main():
+# t = int(input())
+# for _ in range(t):
+    solve()
+if __name__ == "__main__":
+    threading.Thread(target=main).start()
