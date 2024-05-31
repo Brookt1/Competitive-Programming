3319cd837beec01296ae8e85cc7ed1fbe1b93123
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-05-31 15:36:22 +0300

    Created 'C - Palindromic Sum Representations' by A2SV Hub

---
+++ A2SV G5 - Contest #17 31-May-2024/C - Palindromic Sum Representations 162719.py
@@ -0,0 +1,35 @@
+# Problem: C - Palindromic Sum Representations - https://codeforces.com/gym/524965/problem/C
+
+
+
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+
+
+
+def solve():
+
+    max_len = int(1e4) * 4  + 1
+    mod = int(1e9) + 7
+
+    nums = [0] * ( max_len)
+    nums[0] = 1
+    coins = []
+    for i in range(1, max_len):
+        if str(i)  == str(i)[::-1]:
+            coins.append(i)
+
+    for coin in coins:
+        for i in range(coin, max_len):
+            nums[i] = (nums[i] + nums[i - coin])%mod
+            
+
+    for _ in range(int(input())):
+        n = int(input())
+        print((nums[n]))
+ 
+solve()
+
