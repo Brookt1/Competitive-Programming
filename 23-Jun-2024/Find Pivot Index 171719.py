4def9620d768251b3b5a50b46332e306313a29b2
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-06-23 14:23:58 +0300

    Created 'Find Pivot Index' by A2SV Hub

---
+++ 23-Jun-2024/Find Pivot Index 171719.py
@@ -0,0 +1,15 @@
+# Problem: Find Pivot Index - https://leetcode.com/problems/find-pivot-index/
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+
+
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
+        
+        for i in range(1, len(prefix)):
+            if prefix[i - 1] == prefix[-1] - prefix[i]:
+                return i -  1
+        return -1
