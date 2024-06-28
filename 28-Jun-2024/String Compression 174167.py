365f5f926dde74fa9e50c8ddba3b8b7e1f231478
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-06-28 21:27:53 +0300

    Created 'String Compression' by A2SV Hub

---
+++ 28-Jun-2024/String Compression 174167.py
@@ -0,0 +1,27 @@
+# Problem: String Compression - https://leetcode.com/problems/string-compression/
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) == 1:
+            return len(chars)
+        next = 0
+        start = chars[0]
+        curr = 1
+        count = 1
+        while curr <= len(chars):
+            if curr < len(chars) and chars[curr] == start:
+                count += 1
+            else:
+                chars[next] = start
+                next += 1
+                if count > 1:
+                    i = 0
+                    count_str = str(count)
+                    while i < len(count_str):
+                        chars[next] = count_str[i]
+                        next += 1
+                        i += 1
+                start = chars[curr] if curr < len(chars) else None
+                count = 1
+            curr += 1
+        return len(chars[:next])
