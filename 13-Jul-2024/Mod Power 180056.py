cabcb90f93a394e4933fcd60d1538e4513848fc8
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-07-13 13:15:12 -0700

    Created 'Mod Power' by A2SV Hub

---
+++ 13-Jul-2024/Mod Power 180056.py
@@ -0,0 +1,10 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+
+p = pow(a, b)
+print(p)
+print(p % c)
