98a20aa8d92e705ad003fb59aefd64c35f11f626
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-07-24 07:16:20 -0700

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 24-Jul-2024/B - Mohammed's Magical Cleaning Machine 184170.py
@@ -0,0 +1,40 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    
+    n = int(input())
+    nums = intl()
+    non_zero = 0
+    zero = 0
+    for num in nums[:n - 1]:
+        if num:
+            non_zero += num
+        if non_zero and num == 0:
+            zero += 1
+    if non_zero:
+        print(non_zero + zero)
+    else:
+        print(zero)
+
+        
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        solve()
+if __name__ == "__main__":
+    main()
+    # sys.setrecursionlimit(10 **8 )
+    # threading.stack_size(2 * 10 ** 8)
+    
+    # main_thread = threading.Thread(target=main)
+    # main_thread.start()
+    # main_thread.join()
