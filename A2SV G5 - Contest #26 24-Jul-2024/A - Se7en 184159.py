a157236b483c40ea3b22e7fe514ac3b429906b11
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-07-24 06:49:58 -0700

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 24-Jul-2024/A - Se7en 184159.py
@@ -0,0 +1,32 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_left, bisect_right
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+intl = lambda: list(map(int, input().split()))
+
+def solve():
+    n = int(input())
+    if n % 7 == 0:
+        print(n)
+    else:
+        n = int(str(n)[:len(str(n)) - 1] + '0')
+        while n % 7 != 0:
+            n += 1
+        print(n)
+        
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        solve()
+if __name__ == "__main__":
+    main()
+    # sys.setrecursionlimit(10 **8 )
+    # threading.stack_size(2 * 10 ** 8)
+    
+    # main_thread = threading.Thread(target=main)
+    # main_thread.start()
+    # main_thread.join()
