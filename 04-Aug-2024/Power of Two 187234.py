9f615bf9956a361536a1bcf0b570c949c818f7e7
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-08-04 04:18:12 -0700

    Created 'Power of Two' by A2SV Hub

---
+++ 04-Aug-2024/Power of Two 187234.py
@@ -0,0 +1,9 @@
+# Problem: Power of Two - https://leetcode.com/problems/power-of-two/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        
+
+        while n > 1 and n % 2 == 0:
+            n //= 2
+        return n == 1
